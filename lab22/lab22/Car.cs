08123590ee66e48eed279e8788099a7b232998bf
PashaAllahverdiyev <129064604+PashaAllahverdiyev@users.noreply.github.com>
2024-04-09 10:15:22 +0400

    Add files via upload

---
+++ lab22/lab22/Car.cs
@@ -0,0 +1,37 @@
+﻿namespace lab22
+{
+    public class Car:Vehicle 
+
+    {
+        public int FuelCapacity;
+
+        public Car(string brand, string model, int millage, int currentfuel,int fuelcapacity) : base(brand, model, millage, currentfuel)
+        {
+            FuelCapacity = fuelcapacity;
+        }
+
+        public void AddFuel()
+        {
+            
+            Console.WriteLine("Brand: ");
+            string brand = Console.ReadLine();
+            Console.WriteLine("Model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine();
+            l1:
+            Console.WriteLine("Bakda olan benzin");
+            int currentfuel = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Benzin elave edin");
+            int fuel =Convert.ToInt32(Console.ReadLine());
+            if (fuel + currentfuel > FuelCapacity)
+            {
+                Console.WriteLine("Limiti kecdi");
+                goto l1;
+            }
+            else
+            {
+                Console.WriteLine("Benzin elave edildi");
+            }
+        }
+    }
+}
---
+++ lab22/lab22/Program.cs
@@ -0,0 +1,25 @@
+﻿using lab22;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle vehicle = new Vehicle("Bmw","f30",15000,30);
+        Car car = new Car("Bmw", "f30", 15000, 30,50);
+        car.AddFuel();
+    }
+}
+public class Vehicle
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Millage { get; set;  }
+    public double CurrentFuel { get; set; }
+    public Vehicle(string brand,string model,int millage,int currentfuel)
+    {
+        Brand = brand;
+        Model = model;
+        Millage = millage;
+        CurrentFuel = currentfuel;
+    }
+}
